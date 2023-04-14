b79d1cafab2d8d5a1187f70c040ddfbf4821ef10
Émerson M. Luz <emersonmluz@gmail.com>
2023-04-14 16:37:01 -0300

    Criado PomodoroViewController e o router para essa classe

---
+++ kit-tools/Pomodoro/PomodoroCoordinator.swift
@@ -0,0 +1,25 @@
+//
+//  PomodoroCoordinator.swift
+//  kit-tools
+//
+//  Created by Émerson M Luz on 14/04/23.
+//
+
+import UIKit
+
+class PomodoroCoordinator: CoordinatorProtocol {
+    var navigation: UINavigationController?
+    var router: RouterProtocol?
+    
+    init(navigation: UINavigationController? = nil, router: RouterProtocol? = Router.shared) {
+        self.navigation = navigation
+        self.router = router
+    }
+    
+    func start() {
+        let controller = PomodoroViewController()
+        navigation?.pushViewController(controller, animated: true)
+    }
+    
+    
+}
---
+++ kit-tools/Pomodoro/PomodoroViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PomodoroViewController.swift
+//  kit-tools
+//
+//  Created by Émerson M Luz on 14/04/23.
+//
+
+import UIKit
+
+class PomodoroViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
